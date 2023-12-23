2134f8bc28171f2f3b2b39b0fda89f92799553de
epitacio <epitacio.sobrinho00@hotmail.com>
2023-12-23 17:45:44 -0300

    update script ts-test

--- test/unit/teste2.test.ts
+++
@@ -1,6 +0,0 @@
-import test from "node:test";
-import assert from "node:assert";
-
-test("test 2", () => {
-  assert.strictEqual(2, 2);
-});
---
+++ test/utils/multiplication2.test.ts
@@ -0,0 +1,9 @@
+import assert from "node:assert";
+import test from "node:test";
+
+test("teste 2", () => {
+  // let five = new Dollar(5);
+  // five.times(2);
+
+  assert.equal(10, 10);
+});
